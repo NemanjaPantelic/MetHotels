f5935dccf3b18e9acdd88b70cc2dca330bfffae8
Nemanja Pantelic <nemanja.pantelic.3274@metropolitan.ac.rs>
2019-10-06 21:54:16 +0200

    IT255-DZ01

---
+++ index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>IT255-DZ01</title>
+  <meta name="Nemanja" content="IT255-DZ01">
+
+</head>
+
+<body>
+  <?php echo "Hello World"; ?>
+</body>
+</html>
+
+
